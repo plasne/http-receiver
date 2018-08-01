6d2bcfb0ab86f7bdd923c10e42460932cd9de508
Peter <plasne@iname.com>
2018-08-01 08:33:03 -0400

    added response-code

--- receiver.js
+++ receiver.js
@@ -9,11 +9,13 @@ cmd
     .version("0.1.0")
     .option("-p, --port <integer>", `PORT. The port to host the web services on. Defaults to "8100".`, parseInt)
     .option("-f, --format <string>", `FORMAT. The format of the body ("text" or "json"). Defaults to "json".`, /^(text|json)$/i)
+    .option("-r, --response-code <integer>", `RESPONSE_CODE. The HTTP response code that should be returned.`, parseInt)
     .parse(process.argv);
 
 // globals
-const port       = cmd.port       || process.env.PORT                || 8100;
-const format     = cmd.format     || process.env.FORMAT              || "json";
+const port         = cmd.port         || process.env.PORT          || 8100;
+const format       = cmd.format       || process.env.FORMAT        || "json";
+const responseCode = cmd.responseCode || process.env.RESPONSE_CODE || 200;
 
 // log
 console.log(`FORMAT = "${format}"`);
@@ -44,7 +46,7 @@ app.all("/", (req, res) => {
     console.log(req.headers);
     console.log(req.body);
     console.log("");
-    res.status(200).end();
+    res.status(responseCode).end();
 });
 
 // listen for web traffic
